4876a0f231da6eb8a02706fa8029404d6b8ccaf2
AngelinaShamanova <shamanovaaaa@yandex.ru>
2023-02-16 15:21:58 +0100

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Ангелина Шаманова on 16.2.23..
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
